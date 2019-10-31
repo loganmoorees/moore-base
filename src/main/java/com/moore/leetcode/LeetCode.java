5b3b543401e1547bb25c40dbc2b8f2970713c2ce
moore <793665514@qq.com>
2019-10-31 23:41:44 +0800

    commit

---
+++ src/main/java/com/moore/leetcode/LeetCode.java
@@ -0,0 +1,31 @@
+package com.moore.leetcode;
+
+import java.util.List;
+
+/**
+ * LeetCode 题目
+ *
+ * @author moore
+ * @date 2019-10-31
+ */
+public class LeetCode {
+
+    /**
+     * 给定一个包含 n 个整数的数组 nums，判断 nums 中是否存在三个元素 a，b，c ，使得 a + b + c = 0 ？找出所有满足条件且不重复的三元组。
+     * 例如, 给定数组 nums = [-1, 0, 1, 2, -1, -4]，
+     *
+     * 满足要求的三元组集合为：
+     * [
+     *   [-1, 0, 1],
+     *   [-1, -1, 2]
+     * ]
+     *
+     * 题解：采用三指针的解法
+     * 1. 先排序
+     *
+     * @return
+     */
+    public List<List<Integer>> addThreeNumbers() {
+        return null;
+    }
+}
